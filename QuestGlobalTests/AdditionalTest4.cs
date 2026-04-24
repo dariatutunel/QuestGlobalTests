bb5e9e394d231533e30da19b8ea813423725bd45
Daria Tutunel <dariatutunel26@gmail.com>
2026-04-24 14:47:16 +0300

    Additional Test 4 & 5

---
+++ QuestGlobalTests/AdditionalTest4.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Playwright;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace QuestGlobalTests
+{
+    public class AdditionalTest4
+    {
+        [Test]
+        public async Task VerifySessionPersistenceOnReload()
+        {
+            //1. Launch Browser & go to website
+            using var playwright = await Playwright.CreateAsync();
+            await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = false, SlowMo = 500 });
+            var page = await browser.NewPageAsync();
+            await RemovePopUp.AdBlock(page); //Blocks ads
+            await page.GotoAsync("https://automationexercise.com/products");
+            await RemovePopUp.PopUp(page); //Consents if PopUp appears
+
+            //2. Add product to cart
+            var firstAddToCart = page.Locator(".add-to-cart").First;
+            await firstAddToCart.ClickAsync();
+            var viewCartLink = page.Locator("u:has-text('View Cart')");
+            await viewCartLink.ClickAsync();
+
+            //3. Verify product is in cart
+            var cartRows = page.Locator("#cart_info_table tbody tr");
+            await Assertions.Expect(cartRows).ToHaveCountAsync(1);
+            string productNameBefore = await page.Locator(".cart_description h4 a").First.InnerTextAsync();
+
+            //4. Reload page
+            await page.ReloadAsync();
+            await page.WaitForLoadStateAsync(LoadState.NetworkIdle);
+
+            //5. Verify that the product is still in cart
+            await Assertions.Expect(cartRows).ToHaveCountAsync(1);
+            string productNameAfter = await page.Locator(".cart_description h4 a").First.InnerTextAsync();
+            Assert.That(productNameBefore, Is.EqualTo(productNameAfter), "Cart is empty after reload!");
+
+        }
+    }
+}
---
+++ QuestGlobalTests/AdditionalTest5.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Playwright;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace QuestGlobalTests
+{
+    public class AdditionalTest5
+    {
+        [Test]
+        public async Task VerifyCartSyncAcrossTabs()
+        {
+            //1. Setup one session and 2 tabs
+            using var playwright = await Playwright.CreateAsync();
+            await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = false, SlowMo = 500 });
+            var context = await browser.NewContextAsync();
+            var tab1 = await context.NewPageAsync();
+            var tab2 = await context.NewPageAsync();
+            await RemovePopUp.AdBlock(tab1); //Blocks ads
+            await RemovePopUp.AdBlock(tab2); //Blocks ads
+
+            //2. Actions in Tab 1
+            await tab1.GotoAsync("https://automationexercise.com/products");
+            await RemovePopUp.PopUp(tab1); //Consents if PopUp appears
+            var firstProduct = tab1.Locator(".productinfo p").First;
+            string productTab1= await firstProduct.InnerTextAsync();
+            var addToCart = tab1.Locator(".add-to-cart").First;
+            await addToCart.ClickAsync();
+            await tab1.WaitForTimeoutAsync(1000);
+
+            //3. Verify Tab 2 to sync
+            await tab2.GotoAsync("https://automationexercise.com/view_cart");
+            await RemovePopUp.PopUp(tab2); //Consents if PopUp appears
+            var cartRows = tab2.Locator("#cart_info_table tbody tr");
+            await Assertions.Expect(cartRows).ToHaveCountAsync(1);
+            string productTab2 = await tab2.Locator(".cart_description h4 a").First.InnerTextAsync();
+            Assert.That(productTab1, Is.EqualTo(productTab2), "Tabs do not sync!");
+
+        }
+    }
+}
+           
