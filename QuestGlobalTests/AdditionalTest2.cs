654e40a56aced898d3700ab502137568c54b630e
Daria Tutunel <dariatutunel26@gmail.com>
2026-04-24 13:32:08 +0300

    Additional Test 2 & 3

---
+++ QuestGlobalTests/AdditionalTest2.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Playwright;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace QuestGlobalTests
+{
+    public class AdditionalTest2
+    {
+        [Test]
+        public async Task VerifyLinksInMenu()
+        {
+            //1. Launch Browser & go to website
+            using var playwright = await Playwright.CreateAsync();
+            await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = false, SlowMo = 500 });
+            var page = await browser.NewPageAsync();
+            await RemovePopUp.AdBlock(page); //Blocks ads
+            await page.GotoAsync("https://automationexercise.com/");
+            await RemovePopUp.PopUp(page); //Consents if PopUp appears
+
+            await page.ClickAsync("a[href='/login']");
+            await RemovePopUp.PopUp(page); //Consents if PopUp appears
+            await Assertions.Expect(page.Locator("h2:has-text('Login to your account')")).ToBeVisibleAsync();
+            await page.FillAsync("input[data-qa='login-email']", "dariatutunel26@gmail.com");
+            await page.FillAsync("input[data-qa='login-password']", "1234");
+            await page.ClickAsync("button[data-qa='login-button']");
+
+            //2. Extract all the links from the menu
+            var menuLinks = page.Locator(".shop-menu.pull-right ul li a");
+            int linkCount = await menuLinks.CountAsync();  
+            List<string> urllist = new List<string>();
+            for(int i=0;  i<linkCount; i++)
+            {
+                string? href = await menuLinks.Nth(i).GetAttributeAsync("href");
+                if (!string.IsNullOrEmpty(href) )
+                {
+                    if(href.StartsWith("/"))
+                    {
+                        href = "https://automationexercise.com" + href;
+                    }
+                    urllist.Add(href);
+                }
+            }
+
+            //3. Test each link 
+            var apiContext = await playwright.APIRequest.NewContextAsync();
+            Assert.Multiple(async () =>
+            {
+                foreach (string url in urllist)
+                {
+                    var response = await apiContext.GetAsync(url);
+                    Assert.That(response.Ok, Is.True, $"[Broken link detected!]: {url} has returned error code: {response.Status}");
+                }
+            });
+
+        }
+    }
+}
---
+++ QuestGlobalTests/AdditionalTest3.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Playwright;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace QuestGlobalTests
+{
+    public class AdditionalTest3
+    {
+        [Test]
+        public async Task MobileViewportResponsiveness()
+        {
+            //1. Create a virtual phone
+            using var playwright = await Playwright.CreateAsync();
+            var iphone = playwright.Devices["iPhone 13"];
+            await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = false, SlowMo = 500 });
+            var context = await browser.NewContextAsync();
+            var page = await context.NewPageAsync();
+            await RemovePopUp.AdBlock(page); //Blocks ads
+            await page.GotoAsync("https://automationexercise.com/");
+            await RemovePopUp.PopUp(page); //Consents if PopUp appears
+
+            //2. Standard Desktop Menu should be replaced by a "Hamburger" Menu button
+            var desktopMenu = page.Locator(".shop-menu ul");
+            await Assertions.Expect(desktopMenu).Not.ToBeVisibleAsync();
+            var hamburgerMenuIcon = page.Locator(".navbar-toggle");
+            await Assertions.Expect(hamburgerMenuIcon).ToBeVisibleAsync();
+
+            //3. Verify "Hamburger" Menu responsiveness
+            await hamburgerMenuIcon.ClickAsync();
+            await page.WaitForTimeoutAsync(1000);
+            var mobileHomeLink = page.Locator(".navbar-collapse ul li a:has-text('Home')").First;
+            await Assertions.Expect(mobileHomeLink).ToBeVisibleAsync();
+            await page.WaitForTimeoutAsync(1000);
+        }
+    }
+}
